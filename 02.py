539a157b05acd74f602d19a7eed2890f41a305d7
Pier-Luc Brault <pier-luc@brault.me>
2024-12-02 20:44:58 -0500

    Part 1 complete

---
+++ 02.py
@@ -0,0 +1,31 @@
+def is_increasing(report):
+  for i in range(len(report) - 1):
+    if report[i] >= report[i + 1]:
+      return False
+  return True
+
+def is_decreasing(report):
+  for i in range(len(report) - 1):
+    if report[i] <= report[i + 1]:
+      return False
+  return True
+
+def is_gradual(report):
+  for i in range(len(report) - 1):
+    if abs(report[i] - report[i + 1]) > 3:
+      return False
+  return True
+
+reports = []
+safe_reports_count = 0
+
+with open('input.txt', 'r') as input_file:
+  for line in input_file:
+    levels = [int(part) for part in line.split()]
+    reports.append(levels)
+
+for report in reports:
+  if (is_increasing(report) or is_decreasing(report)) and is_gradual(report):
+    safe_reports_count += 1
+
+print('Safe reports count:', safe_reports_count)
