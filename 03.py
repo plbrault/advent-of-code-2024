a143583ac9ea887651a429ae122a81fd3f08492b
Pier-Luc Brault <pier-luc@brault.me>
2024-12-03 22:16:20 -0500

    Part 1 complete

--- 03.py
+++ 03.py
@@ -8,4 +8,9 @@ with open('input.txt', 'r') as file:
 multiply_pattern = r'mul\(\d{1,3},\d{1,3}\)'
 matches = re.findall(multiply_pattern, program_content)
 
-print(matches)
+sum = 0
+for match in matches:
+  numbers = re.findall(r'\d{1,3}', match)
+  sum += int(numbers[0]) * int(numbers[1])
+
+print("Sum of all multiplications:", sum)
