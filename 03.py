b88304afbc9562eb897718ac59c4d08ebfb428e5
Pier-Luc Brault <pier-luc@brault.me>
2024-12-03 22:30:49 -0500

    Part 2 complete

--- 03.py
+++ 03.py
@@ -1,16 +1,19 @@
 import re
 
 program_content = ''
-
 with open('input.txt', 'r') as file:
   program_content = file.read()
 
+program_parts_between_do_instructions = program_content.split('do()')
+
 multiply_pattern = r'mul\(\d{1,3},\d{1,3}\)'
-matches = re.findall(multiply_pattern, program_content)
-
 sum = 0
-for match in matches:
-  numbers = re.findall(r'\d{1,3}', match)
-  sum += int(numbers[0]) * int(numbers[1])
 
-print("Sum of all multiplications:", sum)
+for part in program_parts_between_do_instructions:
+  enabled_program_part = part.split('don\'t()')[0]
+  matches = re.findall(multiply_pattern, enabled_program_part)
+  for match in matches:
+    numbers = re.findall(r'\d{1,3}', match)
+    sum += int(numbers[0]) * int(numbers[1])
+
+print("Sum of all enabled multiplications:", sum)
