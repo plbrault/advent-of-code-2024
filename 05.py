03d92c3bea8359003b09979f3a46268fd2d5afa6
Pier-Luc Brault <pier-luc@brault.me>
2024-12-05 22:29:23 -0500

    Structure ordering rules

--- 05.py
+++ 05.py
@@ -1,11 +1,15 @@
-ordering_rules = []
+class OrderingRule:
+  is_before: [int] = []
+  is_after: [int] = []
+
+raw_ordering_rules = []
 page_updates = []
 
 with open('input.txt', 'r') as file:
   line = file.readline()
   position = file.tell()
   while line.strip() != '':
-    ordering_rules.append([
+    raw_ordering_rules.append([
       int(element) for element in line.split('|')
     ])
     line = file.readline()
@@ -16,6 +20,18 @@ with open('input.txt', 'r') as file:
       int(element) for element in line.split(',')
     ])
 
-print(page_updates)
+ordering_rules = {}
 
-    
+for raw_ordering_rule in raw_ordering_rules:
+  [first_number, second_number] = raw_ordering_rule
+  if first_number not in ordering_rules:
+    ordering_rules[first_number] = OrderingRule()
+  if second_number not in ordering_rules:
+    ordering_rules[second_number] = OrderingRule()
+  ordering_rules[first_number].is_before.append(second_number)
+  ordering_rules[second_number].is_after.append(first_number)
+
+for rule in ordering_rules:
+  print('IS BEFORE', rule, ordering_rules[rule].is_before)
+  print('IS AFTER', rule, ordering_rules[rule].is_after)
+  print('-------------------------------------------------')
