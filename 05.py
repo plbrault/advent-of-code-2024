76e4d7ab2b2552e6c32bd7f12ef4bafc89d92e44
Pier-Luc Brault <pier-luc@brault.me>
2024-12-05 23:19:00 -0500

    Fix compare function

--- 05.py
+++ 05.py
@@ -1,8 +1,6 @@
 from functools import cmp_to_key
 
 class OrderingRule:
-  is_before: [int]
-  is_after: [int]
   def __init__(self):
     self.is_before = []
     self.is_after = []
@@ -12,14 +10,11 @@ page_updates = []
 
 with open('input.txt', 'r') as file:
   line = file.readline()
-  position = file.tell()
   while line.strip() != '':
     raw_ordering_rules.append([
       int(element) for element in line.split('|')
     ])
     line = file.readline()
-    position = file.tell()
-  file.seek(position)
   for line in file:
     page_updates.append([
       int(element) for element in line.split(',')
@@ -28,7 +23,7 @@ with open('input.txt', 'r') as file:
 ordering_rules = {}
 
 for raw_ordering_rule in raw_ordering_rules:
-  [first_number, second_number] = raw_ordering_rule
+  first_number, second_number = raw_ordering_rule
   if first_number not in ordering_rules:
     ordering_rules[first_number] = OrderingRule()
   if second_number not in ordering_rules:
@@ -37,9 +32,9 @@ for raw_ordering_rule in raw_ordering_rules:
   ordering_rules[second_number].is_after.append(first_number)
 
 def compare_page_numbers(x, y):
-  if x in ordering_rules[y].is_before:
-    return -1
   if y in ordering_rules[x].is_before:
+    return -1
+  if x in ordering_rules[y].is_before:
     return 1
   return 0
 
@@ -47,9 +42,7 @@ correct_updates = []
 
 for page_update in page_updates:
   ordered_update = sorted(page_update, key=cmp_to_key(compare_page_numbers))
-  if ordered_update[0] == page_update[0]:
-    print(ordered_update, '!!!!!!!!!!!!!!', page_update)
-  #if ordered_update == page_update:
-    #correct_updates.append(page_update)
+  if ordered_update == page_update:
+    correct_updates.append(page_update)
 
 print('Correct updates:', correct_updates)
