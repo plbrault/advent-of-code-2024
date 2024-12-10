b8dabbd210e929fc163780f504da1719271ee380
Pier-Luc Brault <pier-luc@brault.me>
2024-12-09 22:46:29 -0500

    Create list of blocks

--- 09.py
+++ 09.py
@@ -1,5 +1,29 @@
-numbers: list[int]
+
+from dataclasses import dataclass
+from typing import Optional
+
+disk_map: list[int]
 
 with open('input.txt', 'r') as file:
     first_line = file.readline().strip()
-    numbers = [int(number) for number in first_line]
+    disk_map = [int(number) for number in first_line]
+
+@dataclass
+class Block:
+    file_id: Optional[int] = None
+    is_free_space: bool = False
+    def __repr__(self):
+        if self.is_free_space:
+            return '.'
+        return str(self.file_id)
+
+blocks = []
+
+for i in range(0, len(disk_map), 2):
+    for j in range(disk_map[i]):
+        blocks.append(Block(file_id=int(i/2)))
+    if i + 1 < len(disk_map):
+        for j in range(disk_map[i+1]):
+            blocks.append(Block(is_free_space=True))
+
+print(blocks)
