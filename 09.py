ba9e02591a09f8c64315911f0bd70cf3876d2ee8
Pier-Luc Brault <pier-luc@brault.me>
2024-12-09 23:04:10 -0500

    More refactor

--- 09.py
+++ 09.py
@@ -2,12 +2,6 @@
 from dataclasses import dataclass
 from typing import Optional
 
-disk_map: list[int]
-
-with open('input.txt', 'r') as file:
-    first_line = file.readline().strip()
-    disk_map = [int(number) for number in first_line]
-
 @dataclass
 class Block:
     file_id: Optional[int] = None
@@ -17,7 +11,7 @@ class Block:
             return '.'
         return str(self.file_id)
 
-def get_blocks():
+def get_blocks(disk_map):
   blocks = []
   for i in range(0, len(disk_map), 2):
       for j in range(disk_map[i]):
@@ -27,9 +21,18 @@ def get_blocks():
               blocks.append(Block(is_free_space=True))
   return blocks
 
+def get_checksum(blocks: [Block]):
+  return sum([block.file_id * i for i, block in enumerate(blocks) if block.file_id is not None])
+
+disk_map: list[int]
+
+with open('input.txt', 'r') as file:
+    first_line = file.readline().strip()
+    disk_map = [int(number) for number in first_line]
+
 # Part 1
 
-blocks = get_blocks()
+blocks = get_blocks(disk_map)
 
 previous_free_block_id = 0
 for i in range(len(blocks) - 1, -1, -1):
@@ -40,6 +43,10 @@ for i in range(len(blocks) - 1, -1, -1):
             previous_free_block_id = j
             break
 
-checksum = sum([block.file_id * i for i, block in enumerate(blocks) if block.file_id is not None])
+checksum = get_checksum(blocks)
 
 print('Part 1 Checksum:', checksum)
+
+# Part 2
+
+blocks = get_blocks(disk_map)
