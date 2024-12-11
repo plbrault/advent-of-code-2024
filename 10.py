07746813f4a0d41a5f1dda7a5d216e8e23c7fb98
Pier-Luc Brault <pier-luc@brault.me>
2024-12-10 22:44:42 -0500

    Incorrect solution, does not count each 9 only once

--- 10.py
+++ 10.py
@@ -6,3 +6,31 @@ with open('input.txt', 'r') as file:
         for char in line.strip():
             map_row.append(int(char))
         map.append(map_row)
+
+def walk_trail(x, y) -> int:
+    height = map[y][x]
+    if height == 9:
+        return 1
+
+    score = 0
+    # left
+    if x > 0 and map[y][x - 1] == height + 1:
+        score += walk_trail(x - 1, y)
+    # up
+    if y > 0 and map[y - 1][x] == height + 1:
+        score += walk_trail(x, y - 1)
+    # right
+    if x < len(map[y]) - 1 and map[y][x + 1] == height + 1:
+        score += walk_trail(x + 1, y)
+    # down
+    if y < len(map) - 1 and map[y + 1][x] == height + 1:
+        score += walk_trail(x, y + 1)
+    return score
+
+total_score = 0
+for y in range(len(map)):
+    for x in range(len(map[y])):
+        if map[y][x] == 0:
+            total_score += walk_trail(x, y)
+
+print('Sum of scores for all trailheads:', total_score)
