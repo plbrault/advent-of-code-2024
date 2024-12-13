d53c581938e5267134e2e35af900897813c0c318
Pier-Luc Brault <pier-luc@brault.me>
2024-12-12 22:10:54 -0500

    Revert "Use math to get number of digits and number split"
    This reverts commit 59b745fc36124581ccf8b8360d02f98ce935a8cc.

--- 11.py
+++ 11.py
@@ -1,17 +1,8 @@
-import math
-from datetime import datetime
-
 stones : [int]
 
 with open('input.txt') as file:
     line = file.readline()
     stones = [int(number) for number in line.split(' ')]
-
-def get_number_of_digits(number):
-    return int(math.log10(number)) + 1
-
-def split_number(number_of_digits, number):
-    return number // 10 ** (number_of_digits // 2), number % 10 ** (number_of_digits // 2)
 
 def blink(stones):
     new_stones = []
@@ -19,26 +10,15 @@ def blink(stones):
         if stone == 0:
             new_stones.append(1)
         else:
-            number_of_digits = get_number_of_digits(stone)
-            if number_of_digits % 2 == 0:
-                left_stone, right_stone = split_number(number_of_digits, stone)
-                new_stones.append(left_stone)
-                new_stones.append(right_stone)
+            stone_str = str(stone)
+            if len(stone_str) % 2 == 0:
+                new_stones.append(int(stone_str[:len(stone_str)//2]))
+                new_stones.append(int(stone_str[len(stone_str)//2:]))
             else:
                 new_stones.append(stone * 2024)
     return new_stones
 
 for i in range(25):
-    print('Blink #', i)
     stones = blink(stones)
 
 print('Number of stones after 25 blinks:', len(stones))
-
-last_blink = datetime.now()
-for i in range(50):
-    print('Blink #', i + 25)
-    stones = blink(stones)
-    print('Number of stones:', len(stones))
-    print('Blink duration:', datetime.now() - last_blink)
-
-print('Number of stones after 75 blinks:', len(stones))
