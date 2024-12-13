f36f7d2771963827c133064aa0419051cf87e926
Pier-Luc Brault <pier-luc@brault.me>
2024-12-13 08:08:15 -0500

    Revert "Refactor to use Stone class"
    This reverts commit 0f5e0fd56741ee932a27a614999b9f2ae4670645.

--- 11.py
+++ 11.py
@@ -1,59 +1,24 @@
-
-from dataclasses import dataclass
-import math
-
-NUMBER_OF_BLINKS = 25
-
-@dataclass
-class Stone:
-    value: int
-    remaining_blinks: int
-
-stones : [Stone]
+stones : [int]
 
 with open('input.txt') as file:
     line = file.readline()
-    stones = [Stone(int(value), NUMBER_OF_BLINKS) for value in line.split(' ')]
-
-def get_number_of_digits(number):
-    return int(math.log10(number)) + 1
-
-def split_number(number_of_digits, number):
-    return number // 10 ** (number_of_digits // 2), number % 10 ** (number_of_digits // 2)
+    stones = [int(number) for number in line.split(' ')]
 
 def blink(stones):
     new_stones = []
     for stone in stones:
-        if stone.remaining_blinks == 0:
-            new_stones.append(stone)
-        elif stone.value == 0:
-            new_stones.append(Stone(1, stone.remaining_blinks - 1))
+        if stone == 0:
+            new_stones.append(1)
         else:
-            number_of_digits = get_number_of_digits(stone.value)
-            if number_of_digits % 2 == 0:
-                left_value, right_value = split_number(number_of_digits, stone.value)
-                new_stones.append(
-                    Stone(
-                        left_value,
-                        stone.remaining_blinks - 1
-                    )
-                )
-                new_stones.append(
-                    Stone(
-                        right_value,
-                        stone.remaining_blinks - 1
-                    )
-                )
+            stone_str = str(stone)
+            if len(stone_str) % 2 == 0:
+                new_stones.append(int(stone_str[:len(stone_str)//2]))
+                new_stones.append(int(stone_str[len(stone_str)//2:]))
             else:
-                new_stones.append(
-                    Stone(
-                        stone.value * 2024,
-                        stone.remaining_blinks - 1
-                    )
-                )
+                new_stones.append(stone * 2024)
     return new_stones
 
-for i in range(NUMBER_OF_BLINKS):
+for i in range(25):
     stones = blink(stones)
 
-print('Final number of stones:', len(stones))
+print('Number of stones after 25 blinks:', len(stones))
