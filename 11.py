14c3baae0cf80f3f8ec7340531b9ae8933a6ffc2
Pier-Luc Brault <pier-luc@brault.me>
2024-12-13 08:08:14 -0500

    Revert "WIP precomputations"
    This reverts commit eed5c563a1936ef220a8c20b2edb5e9f43404823.

--- 11.py
+++ 11.py
@@ -2,7 +2,7 @@
 from dataclasses import dataclass
 import math
 
-MAX_BLINKS = 25
+NUMBER_OF_BLINKS = 25
 
 @dataclass
 class Stone:
@@ -13,11 +13,9 @@ stones : [Stone]
 
 with open('input.txt') as file:
     line = file.readline()
-    stones = [Stone(int(value), MAX_BLINKS) for value in line.split(' ')]
+    stones = [Stone(int(value), NUMBER_OF_BLINKS) for value in line.split(' ')]
 
 def get_number_of_digits(number):
-    if number == 0:
-        return 1
     return int(math.log10(number)) + 1
 
 def split_number(number_of_digits, number):
@@ -55,22 +53,7 @@ def blink(stones):
                 )
     return new_stones
 
-precomputations = {}
-for i in range(1, 7):
-    print('####', i)
-    stone = Stone(i * 2024, MAX_BLINKS - 1)
-    blink_count = 1
-    new_stones = [stone]
-    while len([
-        new_stone for new_stone
-            in new_stones
-            if get_number_of_digits(new_stone.value) > 1
-    ]) > 0:
-        new_stones = blink(new_stones)
-        blink_count += 1
-    precomputations[i] = (blink_count, [new_stone.value for new_stone in new_stones])
-
-for i in range(MAX_BLINKS):
+for i in range(NUMBER_OF_BLINKS):
     stones = blink(stones)
 
 print('Final number of stones:', len(stones))
