943fe89af4bc35b3187fb45dc31a787190fe6c1f
Pier-Luc Brault <pier-luc@brault.me>
2024-12-13 21:50:31 -0500

    Correct computing of regions

--- 12.py
+++ 12.py
@@ -1,6 +1,6 @@
 garden = [list(line.strip()) for line in open('input.txt')]
 
-def walk_garden(garden, plot: tuple[int, int], visited_plots: set(tuple[int, int])) -> list[tuple[int, int]]:
+def walk_garden(garden, plot, visited_plots):
     x, y = plot
     region = [plot]
     visited_plots.add(plot)
@@ -8,50 +8,41 @@ def walk_garden(garden, plot: tuple[int, int], visited_plots: set(tuple[int, int
     # up
     if (
         y > 0
-        and garden[y - 1][x] not in visited_plots
+        and (x, y - 1) not in visited_plots
         and garden[y - 1][x] == garden[y][x]
     ):
-        visited_plots.add((x, y - 1))
-        region.append((x, y - 1))
         region += walk_garden(garden, (x, y - 1), visited_plots)
-    #right
+    # right
     if (
         x < len(garden[y]) - 1
-        and garden[y][x + 1] not in visited_plots
+        and (x + 1, y) not in visited_plots
         and garden[y][x + 1] == garden[y][x]
     ):
-        visited_plots.add((x + 1, y))
-        region.append((x + 1, y))
         region += walk_garden(garden, (x + 1, y), visited_plots)
     # down
     if (
         y < len(garden) - 1
-        and garden[y + 1][x] not in visited_plots
+        and (x, y + 1) not in visited_plots
         and garden[y + 1][x] == garden[y][x]
     ):
-        visited_plots.add((x, y + 1))
-        region.append((x, y + 1))
         region += walk_garden(garden, (x, y + 1), visited_plots)
     # left
     if (
         x > 0
-        and garden[y][x - 1] not in visited_plots
+        and (x - 1, y) not in visited_plots
         and garden[y][x - 1] == garden[y][x]
     ):
-        visited_plots.add((x - 1, y))
-        region.append((x - 1, y))
         region += walk_garden(garden, (x - 1, y), visited_plots)
+    
     return region
 
-
-regions : [[(int, int)]] = []
-visited_plots: set[tuple[int, int]] = set()
+regions = []
+visited_plots = set()
 
 for y in range(len(garden)):
     for x in range(len(garden[y])):
         if (x, y) not in visited_plots:
-            regions.append(walk_garden(garden, (x, y), visited_plots))
+            region = walk_garden(garden, (x, y), visited_plots)
+            regions.append(region)
 
 print(len(regions), 'regions')
-print(regions)
-    
