6fe0633a65a5f10bf4baf757b674b1d4a5c35303
Pier-Luc Brault <pier-luc@brault.me>
2024-12-14 00:06:56 -0500

    Correct result

--- 12.py
+++ 12.py
@@ -80,7 +80,7 @@ def has_plot_on_the_left(region_matrix, x, y):
     return x != 0 and region_matrix[y][x - 1] != ''
 
 def has_plot_on_the_right(region_matrix, x, y):
-    return region_matrix[y][x] != ''
+    return x < len(region_matrix[0]) and region_matrix[y][x] != ''
 
 def has_left_side(region_matrix, x, y):
     return not has_plot_on_the_left(region_matrix, x, y) and has_plot_on_the_right(region_matrix, x, y)
@@ -92,7 +92,7 @@ def has_plot_above(region_matrix, x, y):
     return y != 0 and region_matrix[y - 1][x] != ''
 
 def has_plot_below(region_matrix, x, y):
-    return region_matrix[y][x] != ''
+    return y < len(region_matrix) and region_matrix[y][x] != ''
 
 def has_top_side(region_matrix, x, y):
     return not has_plot_above(region_matrix, x, y) and has_plot_below(region_matrix, x, y)
@@ -105,7 +105,7 @@ def count_sides(region):
     sides = 0
     print('!!!!!!!!!!', garden[region[0][1]][region[0][0]])
     # Check for vertical sides
-    for x in range(len(region_matrix[0])):
+    for x in range(len(region_matrix[0]) + 1):
         for y in range(len(region_matrix)):
             above_has_left_side = y != 0 and has_left_side(region_matrix, x, y - 1)
             above_has_right_side = y != 0 and has_right_side(region_matrix, x, y - 1)
@@ -123,7 +123,7 @@ def count_sides(region):
     print('vertical sides', sides)
 
     # Check for horizontal sides
-    for y in range(len(region_matrix)):
+    for y in range(len(region_matrix) + 1):
         for x in range(len(region_matrix[0])):
             left_has_top_side = x != 0 and has_top_side(region_matrix, x - 1, y)
             left_has_bottom_side = x != 0 and has_bottom_side(region_matrix, x - 1, y)
